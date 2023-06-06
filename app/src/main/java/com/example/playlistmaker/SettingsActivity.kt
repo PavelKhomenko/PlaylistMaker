558cf0ec22e71c73f9ea27cd928cd6699d70670f
PavelKhomenko <PavelKhomenko8@gmail.com>
2023-06-06 17:06:40 +0300

    HomeTask

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -8,13 +8,11 @@ import android.view.View
 
 
 class SettingsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val back = findViewById<View>(R.id.back)
-
         back.setOnClickListener {
             val displayMainMenuIntent = Intent(this, MainActivity::class.java)
             startActivity(displayMainMenuIntent)
