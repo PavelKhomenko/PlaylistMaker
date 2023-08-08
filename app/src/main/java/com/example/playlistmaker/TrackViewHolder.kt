5a51b0a7e317ac287a9fe54732a508605fb5bcd7
PavelKhomenko <PavelKhomenko8@gmail.com>
2023-08-08 23:54:26 +0300

    Shared Preferences + small refactoring

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,9 +1,12 @@
 package com.example.playlistmaker
 
+import android.content.SharedPreferences
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
