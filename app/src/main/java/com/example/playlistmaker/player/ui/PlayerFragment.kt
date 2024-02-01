365060b30f76976fd2f21c6080da31776b6709e2
PavelKhomenko <PavelKhomenko8@gmail.com>
2024-02-01 19:57:03 +0300

    Bug Fix

--- app/src/main/java/com/example/playlistmaker/player/ui/PlayerFragment.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlayerFragment.kt
@@ -16,6 +16,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.FragmentLibraryBinding
+import com.example.playlistmaker.databinding.FragmentPlayerBinding
 import com.example.playlistmaker.library.playlists.presentation.PlaylistState
 import com.example.playlistmaker.player.domain.model.Track
 import com.example.playlistmaker.player.presentation.PlayerStatus
@@ -28,44 +30,28 @@ import java.util.Locale
 
 class PlayerFragment : Fragment() {
 
-    private val ivBackButton: ImageView by lazy { requireView().findViewById(R.id.back_arrow) }
-    private val ivSongCover: ImageView by lazy { requireView().findViewById(R.id.cover) }
-    private val tvSongName: TextView by lazy { requireView().findViewById(R.id.song_name) }
-    private val tvSongArtist: TextView by lazy { requireView().findViewById(R.id.song_artist) }
-    private val tvSongDuration: TextView by lazy { requireView().findViewById(R.id.duration) }
-    private val tvSongAlbum: TextView by lazy { requireView().findViewById(R.id.album_name) }
-    private val tvAlbumTitle: TextView by lazy { requireView().findViewById(R.id.album_title) }
-    private val tvSongYear: TextView by lazy { requireView().findViewById(R.id.year) }
-    private val tvSongGenre: TextView by lazy { requireView().findViewById(R.id.genre) }
-    private val tvSongCountry: TextView by lazy { requireView().findViewById(R.id.country) }
-    private val btPlay: ImageView by lazy { requireView().findViewById(R.id.play_button) }
-    private val btLike: ImageView by lazy { requireView().findViewById(R.id.like_button) }
-    private val btPlaylist: ImageView by lazy { requireView().findViewById(R.id.save_button) }
-    private val tvSecondsPassed: TextView by lazy { requireView().findViewById(R.id.time_played) }
-
-    private val overlay: View by lazy { requireView().findViewById(R.id.overlay) }
-    private val bottomSheet: LinearLayout by lazy { requireView().findViewById(R.id.design_bottom_sheet) }
-    private val btAddNewPlaylist: Button by lazy { requireView().findViewById(R.id.btAddNewPlaylist) }
-    private val rvBottomSheet: RecyclerView by lazy { requireView().findViewById(R.id.rvBottomSheet) }
+    private lateinit var track: Track
     private lateinit var bottomSheetAdapter: BottomSheetAdapter
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
-    private var isInsidePlaylist: Boolean = true
 
+    private var isInsidePlaylist: Boolean = true
     private val viewModel by viewModel<PlayerViewModel>()
-    private lateinit var track: Track
+    private var _binding: FragmentPlayerBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_player, container, false)
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        tvSongAlbum.visibility = View.VISIBLE
-        tvAlbumTitle.visibility = View.VISIBLE
+        binding.albumName.visibility = View.VISIBLE
+        binding.albumTitle.visibility = View.VISIBLE
         track = requireArguments().getSerializable(SEARCH_INPUT_KEY) as Track
         getData(track)
         viewModel.isLiked(track)
@@ -101,21 +87,23 @@ class PlayerFragment : Fragment() {
     }
 
     private fun setupBottomSheet() {
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet).apply {
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.designBottomSheet).apply {
             state = BottomSheetBehavior.STATE_HIDDEN
         }
-        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when (newState) {
                     BottomSheetBehavior.STATE_HIDDEN -> {
-                        overlay.visibility = View.GONE
+                        binding.overlay.visibility = View.GONE
                     }
-                    else -> overlay.visibility = View.VISIBLE
+
+                    else -> binding.overlay.visibility = View.VISIBLE
                 }
             }
 
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                overlay.alpha = slideOffset
+                binding.overlay.alpha = slideOffset
             }
         })
     }
@@ -138,23 +126,23 @@ class PlayerFragment : Fragment() {
                 viewModel.addTrackInPlaylist(track)
             }
         }
-        rvBottomSheet.adapter = bottomSheetAdapter
-        rvBottomSheet.layoutManager = LinearLayoutManager(requireContext())
-        rvBottomSheet.adapter?.notifyDataSetChanged()
+        binding.rvBottomSheet.adapter = bottomSheetAdapter
+        binding.rvBottomSheet.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvBottomSheet.adapter?.notifyDataSetChanged()
     }
 
     private fun setupListeners(track: Track) {
-        ivBackButton.setOnClickListener { findNavController().popBackStack()}
-        btPlay.setOnClickListener {
+        binding.backArrow.setOnClickListener { findNavController().popBackStack() }
+        binding.playButton.setOnClickListener {
             viewModel.onBtnPlayClicked()
         }
-        btLike.setOnClickListener {
+        binding.likeButton.setOnClickListener {
             viewModel.onBtnFavoritesClicked(track)
         }
-        btPlaylist.setOnClickListener {
+        binding.saveButton.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
         }
-        btAddNewPlaylist.setOnClickListener {
+        binding.btAddNewPlaylist.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
             findNavController().navigate(R.id.action_playerFragment_to_createPlaylistFragment)
         }
@@ -170,42 +158,43 @@ class PlayerFragment : Fragment() {
         Glide.with(this).load(track.artworkUrl100.replaceAfterLast(DELIMITER, REPLACEMENTS))
             .placeholder(R.drawable.placeholder).centerInside()
             .transform(RoundedCorners(this.resources.getDimensionPixelSize(R.dimen.margin_8dp)))
-            .into(ivSongCover)
+            .into(binding.cover)
 
-        tvSongDuration.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTime)
-        tvSongAlbum.text = track.collectionName.ifEmpty {
+        binding.duration.text =
+            SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTime)
+        binding.albumName.text = track.collectionName.ifEmpty {
             ({
-                tvSongAlbum.visibility = View.GONE
-                tvAlbumTitle.visibility = View.GONE
+                binding.albumName.visibility = View.GONE
+                binding.albumTitle.visibility = View.GONE
             }).toString()
         }
 
-        tvSongName.text = track.trackName
-        tvSongArtist.text = track.artistName
-        tvSongYear.text = track.releaseDate.subSequence(ZERO, FOUR)
-        tvSongGenre.text = track.primaryGenreName
-        tvSongCountry.text = track.country
-        tvSecondsPassed.text = TIMER_BEGIN_TIME
+        binding.songName.text = track.trackName
+        binding.songArtist.text = track.artistName
+        binding.year.text = track.releaseDate.subSequence(ZERO, FOUR)
+        binding.genre.text = track.primaryGenreName
+        binding.country.text = track.country
+        binding.timePlayed.text = TIMER_BEGIN_TIME
     }
 
     private fun setPauseImage() {
-        btPlay.setImageResource(R.drawable.pause)
+        binding.playButton.setImageResource(R.drawable.pause)
     }
 
     private fun setPlayImage() {
-        btPlay.setImageResource(R.drawable.play)
+        binding.playButton.setImageResource(R.drawable.play)
     }
 
     private fun setLikeImage() {
-        btLike.setImageResource(R.drawable.like)
+        binding.likeButton.setImageResource(R.drawable.like)
     }
 
     private fun setDislikeImage() {
-        btLike.setImageResource(R.drawable.dislike)
+        binding.likeButton.setImageResource(R.drawable.dislike)
     }
 
     private fun updateTimePlayed(timePlayed: String) {
-        tvSecondsPassed.text = timePlayed
+        binding.timePlayed.text = timePlayed
     }
 
     companion object {
