b26b1015d732ff7d09edc8f8405582dfb91129b5
PavelKhomenko <PavelKhomenko8@gmail.com>
2024-02-06 18:21:33 +0300

    final_Step4 "Share Playlist Intent"

--- app/src/main/java/com/example/playlistmaker/library/playlistDetails/ui/PlaylistDetailsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/playlistDetails/ui/PlaylistDetailsFragment.kt
@@ -1,11 +1,13 @@
 package com.example.playlistmaker.library.playlistDetails.ui
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -95,6 +97,7 @@ class PlaylistDetailsFragment : Fragment() {
         binding.back.setOnClickListener {
             findNavController().popBackStack()
         }
+        binding.sharePlaylist.setOnClickListener { sharePlaylist() }
 
     }
 
@@ -111,4 +114,32 @@ class PlaylistDetailsFragment : Fragment() {
 
     }
 
+    private fun sharePlaylist() {
+        if (playlist.playlistTracks.isEmpty()) {
+            Toast.makeText(
+                requireContext(),
+                "В этом плейлисте нет списка треков, которым можно поделиться",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            val shareIntentLink = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, getPlaylistDataToShare(playlist, trackInPlaylist))
+                type = "text/plain"
+            }
+            requireActivity().startActivity(shareIntentLink.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        }
+    }
+
+    private fun getPlaylistDataToShare(playlist: Playlist, trackList: List<Track>): String {
+        val dataToShare = mutableListOf<String>()
+        dataToShare.add(playlist.playlistName)
+        if (!playlist.playlistDescription.isNullOrEmpty()) dataToShare.add(playlist.playlistDescription)
+        dataToShare.add(ConvertUlits().convertSizeToString(playlist.playlistSize))
+        trackList.forEachIndexed { index, track ->
+            dataToShare.add(
+                "${index + 1} ${track.artistName} - ${track.trackName}"
+            )
+        }
+        return dataToShare.joinToString("\n")
+    }
 }
